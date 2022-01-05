176c799c69cac8b6bff9616306aa3363c05d77e6
John <spherix@ymail.com>
2022-01-05 17:00:32 -0500

    Update analyzeall.js

--- analyzeall.js
+++ analyzeall.js
@@ -1,12 +1,13 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-    const servers = ["n00dles", "foodnstuff", "sigma-cosmetics", "joesguns", "nectar-net", "hong-fang-tea", 
-        "harakiri-sushi", "neo-net", "zer0", "max-hardware", "iron-gym", "CSEC", "silver-helix", 
-        "crush-fitness", "phantasy", "omega-net", "the-hub", "johnson-ortho", "avmnite-02h", "netlink", 
-        "rothman-uni", "summit-uni", "catalyst", "comptek", "I.I.I.I", "millenium-fitness", "rho-construction", 
-        "syscore", "lexo-corp", "alpha-ent", "aevum-police", "snap-fitness", "global-pharm", "unitalife", 
-        "univ-energy", "zb-def", "nova-med", "zb-institute", "galactic-cyber", "omnia", "icarus", "deltaone", 
-        "solaris", "aerocorp", "defcomm", "zeus-med"];
+    const servers = ["n00dles", "foodnstuff", "sigma-cosmetics", "joesguns", "nectar-net", "hong-fang-tea", "harakiri-sushi",
+        "neo-net", "zer0", "max-hardware", "iron-gym", "CSEC", "silver-helix", "crush-fitness", "phantasy", "omega-net", "the-hub",
+        "johnson-ortho", "avmnite-02h", "netlink", "rothman-uni", "summit-uni", "catalyst", "comptek", "I.I.I.I",
+        "millenium-fitness", "rho-construction", "syscore", "lexo-corp", "alpha-ent", "aevum-police", "snap-fitness", "global-pharm",
+        "unitalife", "univ-energy", "zb-def", "nova-med", "applied-energetics", "zb-institute", "vitalife", "titan-labs",
+        "solaris", "microdyne", "helios", "deltaone", "icarus", "zeus-med", "omnia", "defcomm", "galactic-cyber", "infocomm",
+        "taiyang-digital", "stormtech", "aerocorp", "clarkinc", "omnitek", "nwo", "4sigma", "blade", "b-and-a", "ecorp",
+        "fulcrumtech", "megacorp", "kuai-gong", "fulcrumassets", "powerhouse-fitness", ".", "run4theh111z"];
 
     for (var i = 0; i < servers.length; i++) {
         // max money, current money, current money percentage
